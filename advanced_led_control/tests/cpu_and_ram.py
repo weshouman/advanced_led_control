70aeb0a12b68440eca8bf3f572ea75162bf3ed4b
Walid Shouman <eng.walidshouman@gmail.com>
2020-02-09 00:33:23 +0100

    Expand the cpu and ram example to also check for disks and temperature.

--- advanced_led_control/tests/cpu_and_ram.py
+++ advanced_led_control/tests/cpu_and_ram.py
@@ -10,21 +10,30 @@ import logging
 
 import psutil
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+
+def get_val_ind(num):
+
+	val = ValueIndication('blue', NO_FLICKER)
+
+	if num <= 40:
+		val.col = 'green'
+	elif num <= 60:
+		val.col = 'yellow'
+	elif num <= 80:
+		val.col = 'orange'
+		val.flicker = 1
+	elif num > 80:
+		val.col = 'red'
+		val.flicker = num - 80
+
+	return val
+
 def indic1_func():
 	cpu = psutil.cpu_percent()
 
-	val = ValueIndication('yellow', NO_FLICKER)
-	if cpu <= 40:
-		val.col = 'green'
-	elif cpu <= 60:
-		val.col = 'orange'
-	elif cpu <= 80:
-		val.col = 'red'
-		val.flicker = 1
-	elif cpu > 80:
-		val.col = 'red'
-		val.flicker = cpu - 80
+	val = get_val_ind(cpu)
+	print("cpu: " + str(cpu) +" (" + val.col + ": " + str(val.flicker) + ")")
 
 	return val
 
@@ -32,28 +41,42 @@ def indic2_func():
 	virt_mem = psutil.virtual_memory()
 	mem = virt_mem.percent
 
-	val = ValueIndication('yellow', NO_FLICKER)
-	if mem <= 40:
-		val.col = 'green'
-	elif mem <= 60:
-		val.col = 'orange'
-	elif mem <= 80:
-		val.col = 'red'
-		val.flicker = 1
-	elif mem > 80:
-		val.col = 'red'
-		val.flicker = mem - 80
+	val = get_val_ind(mem)
+	print("mem: " + str(mem) +" (" + val.col + ": " + str(val.flicker) + ")")
+
+	return val
+
+def indic3_func():
+	du = psutil.disk_usage('/').percent
+
+	val = get_val_ind(du)
+	print("du: " + str(du) +" (" + val.col + ": " + str(val.flicker) + ")")
+
+	return val
+
+def indic4_func():
+	f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+	temp_text = f.read()
+	f.close()
+	temp = int(temp_text) / 1000.0
+
+	val = get_val_ind(temp)
+	print("temperature: " + str(temp) +" (" + val.col + ": " + str(val.flicker) + ")")
 
 	return val
 
 indic1 = Indicator(m_col='aqua', func=indic1_func, brightness = 0.1, i_time=4)
 indic2 = Indicator(m_col='orchid', func=indic2_func, brightness = 0.1, i_time=4)
+indic3 = Indicator(m_col='whitesmoke', func=indic3_func, brightness = 0.1, i_time=4)
+indic4 = Indicator(m_col='mistyrose', func=indic4_func, brightness = 0.1, i_time=4)
 
 stick = blinkstick.find_first()
-procedure = Procedure(stick=stick, mode_led=LED_2, quiet=10, sync_duration=0)
+procedure = Procedure(stick=stick, mode_led=LED_2, quiet=10, sync_duration=20)
 
 procedure.indicators.append(indic1)
 procedure.indicators.append(indic2)
+procedure.indicators.append(indic3)
+procedure.indicators.append(indic4)
 
 procedure.run()
 
